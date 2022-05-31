bcda0c9b24923217eb603580362c31399fa20836
Lucas Quintana <66920069+LucasGabrielQuintana@users.noreply.github.com>
2022-05-31 20:15:07 -0300

    practicando switch

---
+++ condicional switch/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Condicional switch\n");
+
+    int option;
+    scanf("%i", &option);
+
+    switch(option)
+    {
+    case 1:
+        printf("Elegiste el numero 1 \n");
+        break;
+    case 2:
+        printf("Elegiste el numero 2 \n");
+        break;
+    case 3:
+        printf("Elegiste el numero 3 \n");
+        break;
+    default:
+        printf("NINGUNO");
+        break;
+    }
+    return 0;
+}
